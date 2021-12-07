60e1a1059c87d66674a75b37d3785cd369c1d546
Long Le <lhlong09t4@gmail.com>
2021-12-07 22:02:18 +0700

    add User page

--- lib/ui/user/user_page.dart
+++ lib/ui/user/user_page.dart
@@ -1,12 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:devfest_2021/ultilize/user_preferences.dart';
+import 'package:devfest_2021/data/models/user/user.dart';
+
 
 class UserPage extends StatelessWidget {
   const UserPage({Key? key}) : super(key: key);
 
+  // final user = UserPreferences.myUser;
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("User Page"),
+    return Center(
+        //
+      child: Builder(
+        builder: (context) => MaterialApp(
+          title: 'User Profile',
+          theme: new ThemeData(scaffoldBackgroundColor: Colors.black),
+          home: ProfilePage(),
+        ),
+      ),
     );
   }
+}
+
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    final user = UserPreferences.myUser;
+
+    return Container(
+      child: Builder(
+        builder: (context) => Scaffold(
+          body: ListView(
+            physics: BouncingScrollPhysics(),
+            children: [
+              buildName(user),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildName(User user) => Column(
+    children: [
+      Text(
+        user.name,
+        style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontSize: 28),
+      ),
+      const SizedBox(height: 4),
+      Text(
+        user.email,
+        style: TextStyle(
+            color: Colors.white
+        ),
+      )
+    ],
+  );
 }
---
+++ lib/ultilize/user_preferences.dart
@@ -0,0 +1,12 @@
+import 'package:devfest_2021/data/models/user/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath:
+    'https://images.unsplash.com/photo-1525779350160-30931cbc2aac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    name: 'John Lh',
+    email: 'john.lh@gmail.com',
+    about:
+    'photographer and hunter',
+  );
+}
